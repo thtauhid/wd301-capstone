439845fcbb581c658598370ac83ff0b097ec3c1e
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-07-25 16:23:31 +0530

    article list converted to raw tailwind

--- src/pages/articles/ArticleCard.tsx
+++ src/pages/articles/ArticleCard.tsx
@@ -1,39 +1,20 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Article } from "@/context/articles/types";
 import { Link } from "react-router-dom";
+import { H3 } from "@/components/ui/heading";
 
 function ArticleCard(props: { article: Article }) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{props.article.title}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className='flex justify-between'>
-          <div>
-            <p>{props.article.summary}</p>
-            <Link to={`/articles/${props.article.id}`}>
-              <Button className='mt-4'>Read More</Button>
-            </Link>
-          </div>
-          <img src={props.article.thumbnail} alt='' className='h-32 w-32' />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <p>
-          {props.article.sport.name} |{" "}
-          {new Date(props.article.date).toDateString()}
-          {/* TODO: make a utility fn that convert the string formatted date to a good looking date */}
-        </p>
-      </CardFooter>
-    </Card>
+    <div className='border flex'>
+      <img src={props.article.thumbnail} alt='' className='h-48 w-48' />
+      <div className='p-4'>
+        <H3>{props.article.title}</H3>
+        <p className='mt-4'>{props.article.summary}</p>
+        <Link to={`/articles/${props.article.id}`}>
+          <Button className='mt-4'>Read More</Button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
