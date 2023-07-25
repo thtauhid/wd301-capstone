3280a1d1a2aac54c9a907530f02b85716e16e4b0
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-07-25 10:40:21 +0530

    added heading component

---
+++ src/components/ui/heading.tsx
@@ -0,0 +1,23 @@
+export const H1 = (props: { children: React.ReactNode }) => {
+  return <h1>{props.children}</h1>;
+};
+
+export const H2 = (props: { children: React.ReactNode }) => {
+  return <h2 className='font-bold text-2xl mx-4'>{props.children}</h2>;
+};
+
+export const H3 = (props: { children: React.ReactNode }) => {
+  return <h3>{props.children}</h3>;
+};
+
+export const H4 = (props: { children: React.ReactNode }) => {
+  return <h4>{props.children}</h4>;
+};
+
+export const H5 = (props: { children: React.ReactNode }) => {
+  return <h5>{props.children}</h5>;
+};
+
+export const H6 = (props: { children: React.ReactNode }) => {
+  return <h6>{props.children}</h6>;
+};
