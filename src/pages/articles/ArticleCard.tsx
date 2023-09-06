85553879fcf38bdd16a6186aa8a3b5bc7d22a7b3
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-09-06 18:27:36 +0530

    made images look square in article list

--- src/pages/articles/ArticleCard.tsx
+++ src/pages/articles/ArticleCard.tsx
@@ -6,8 +6,12 @@ import Button from "@/components/ui/button";
 function ArticleCard(props: { article: Article }) {
   return (
     <div className='border flex'>
-      <img src={props.article.thumbnail} alt='' className='h-48 w-48' />
-      <div className='p-4'>
+      <img
+        src={props.article.thumbnail}
+        alt=''
+        className='h-48 w-48 aspect-square'
+      />
+      <div className='p-4 w-full'>
         <H3>{props.article.title}</H3>
         <p className='mt-4'>{props.article.summary}</p>
         <div className='flex items-end justify-between'>
